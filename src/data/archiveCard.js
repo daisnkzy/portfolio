027171a08726ca700018d392f797a553453708fd
taosir <taosir02@163.com>
2023-09-06 21:38:53 +0800

    修改内容和排版

--- src/data/archiveCard.js
+++ src/data/archiveCard.js
@@ -1,8 +1,8 @@
-export const card1 = {
-  title: "速记卡片",
-  desc: "卡片正面显示问题，背面显示答案。点击卡片时反转卡片。一次只能翻转一张卡片。",
-  listItem: ["React", "三元选择符", "map"],
-  link: "https://7cpcn5.csb.app/",
+export const card5 = {
+  title: "谚语生成",
+  desc: "调用第三方api，点击按钮自动更新谚语。",
+  listItem: ["useEffect", "三元选择符", "fetch api"],
+  link: "https://lyxjf4.csb.app/",
 };
 export const card2 = {
   title: "汇率转换器",
@@ -11,30 +11,30 @@ export const card2 = {
   link: "https://g6sy4s.csb.app/",
 };
 export const card3 = {
-  title: "小费计算器",
-  desc: "根据吃一顿饭的价格，以及你和你朋友的满意程度百分比来决定小费。",
-  listItem: ["React", "三元选择符", "map"],
-  link: "https://dhs5zw-5173.csb.app/",
-};
-export const card4 = {
   title: "Q&A",
   desc: "经典Q&A，点击问题显示回答。",
   listItem: ["props传递数据", "&&选择符", "map"],
   link: "https://st94my.csb.app/",
 };
-export const card5 = {
+export const card9 = {
+  title: "小费计算器",
+  desc: "根据吃一顿饭的价格，以及你和你朋友的满意程度百分比来决定小费。",
+  listItem: ["React", "三元选择符", "map"],
+  link: "https://j5r7cz.csb.app/",
+};
+export const card1 = {
   title: "BMI",
   desc: "通过滑块条来选择自己身高体重，自动计算出bmi，且一用useMemo钩子优化性能。",
   listItem: ["useMemo", "range属性"],
   link: "https://xfsx43.csb.app/",
 };
-export const card6 = {
-  title: "谚语生成",
-  desc: "调用第三方api，点击按钮自动更新谚语。",
-  listItem: ["useEffect", "三元选择符", "fetch api"],
-  link: "https://lyxjf4.csb.app/",
-};
 export const card7 = {
+  title: "速记卡片",
+  desc: "卡片正面显示问题，背面显示答案。点击卡片时反转卡片。一次只能翻转一张卡片。",
+  listItem: ["React", "三元选择符", "map"],
+  link: "https://7cpcn5.csb.app/",
+};
+export const card6 = {
   title: "Github用户搜索",
   desc: "调用github api，输入github用户名显示用户信息。",
   listItem: ["fetch", "axios"],
@@ -46,7 +46,7 @@ export const card8 = {
   listItem: ["Props传递数据", "React 状态提升"],
   link: "https://n7tsw6.csb.app/",
 };
-export const card9 = {
+export const card4 = {
   title: "图片轮播",
   desc: "经典的点击按钮切换图片显示。",
   listItem: ["useState", "三元选择符", "map"],
